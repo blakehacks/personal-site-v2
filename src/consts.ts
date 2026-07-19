12853f12215b1d9a1a40e1868cbe32c9cd5f8a0a
blakehacks <bmaczkawork@gmail.com>
2026-07-18 21:06:20 -0400

    cleanup

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Blake Maczka';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION =
+	'The personal website of Blake Maczka — software engineer and writer in Brooklyn. Projects, writing, and reading lists.';
